53414776bc82c83e5ed2347f3d7eb32acf0b6d3a
Liga Cepurniece <ligacepurniece@gmail.com>
2021-10-29 20:53:43 +0300

    Made Bored

---
+++ src/main/java/Board.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Board {
+    private static JFrame frame;
+    private static String title;
+
+    public Board(int width, int height, int gridcount, String title, Game game){
+        Board.title = title;
+        frame = new JFrame(title);
+
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+}
