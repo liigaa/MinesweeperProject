d710929afc7c62ffdf48d4d7496ff5066301de3d
Andra Balode <andraibalodei@gmail.com>
2021-10-31 12:05:11 +0200

    Handler initial commit

---
+++ src/main/java/Handler.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Handler {
+
+    private ArrayList<Cell> current = new ArrayList<Cell>();
+    private ArrayList<Cell> flood = new ArrayList<Cell>();
+
+    private static int flaggedCells = 0;
+}
